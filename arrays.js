dd5c082ec9c41683d66ad0090fea1b308393765d
Olu Isaac <oluisaac7@gmail.com>
2019-03-19 11:10:25 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -8,12 +8,17 @@ return [element, ...array];
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift()
+  array
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
 array.unshift(element);
 return array;
 }
 
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push (element)
+  return array;
+}
 function AddElementToEndOfArray(array,element){
   return [...array, element]
 }
