d11109a64070672f3ea0af33a2b49e6fb4a0c7ef
Olu Isaac <oluisaac7@gmail.com>
2019-03-19 11:32:57 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,25 +2,41 @@ var = chocolateBars;
 
 var chocolateBars = ["snickers", "hundred grand", "kitkat","skittles"];
 
-function addElementToBeginningOfArray(array, element) {
-return [element, ...array];
+function addElementToBeginningOfArray(array, element){
+  return[Element,array];
 }
 
-function destructivelyRemoveElementFromBeginningOfArray(array) {
+function destructivelyAddElementToBeginningOfArray (array, element){
+  array.unshift (element)
+}
+
+function addElementToEndOfArray (array, element){
+  return[...array,element]
+}
+
+function destructivelyAddElementToEndOfArray (array, element){
+  array.push (element)
+  return array
+}
+
+function destructivelyRemoveElementFromBeginningOfArray (array){
   array.shift()
   array
-
-function destructivelyAddElementToBeginningOfArray(array, element) {
-array.unshift(element);
-return array;
 }
 
-function destructivelyAddElementToEndOfArray(array, element){
-  array.push (element)
-  return array;
-}
-function AddElementToEndOfArray(array,element){
-  return [...array, element]
+function removeElementFromBeginningOfArray (array){
+  return array.slice(1)
 }
 
+function destructivelyRemoveElementFromEndOfArray (array){
+  array.pop ()
+  retunr array
+}
+
+function removeElementFromEndOfArray (array){
+  return array.slice (0,array.length -1);
+}
+
+function accessElementInArray (array, index){
+  return array[index]
 }
