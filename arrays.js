2efb524c14d41c5dd1a0d0d409bf67967f9e6f7d
Olu Isaac <oluisaac7@gmail.com>
2019-03-19 10:57:35 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,16 +1,17 @@
 
-function destructivelyAddElementToBeginningOfArray(array, element) {
-array.unshift(element);
-return array;
-}
-
-function destructivelyRemoveElementFromBeginningOfArray(array, element){
-  return array;
-}
-
 var chocolateBars = ["snickers", "hundred grand", "kitkat","skittles"];
 
 function addElementToBeginningOfArray(array, element) {
 array.unshift(element);
 return [element, ...array];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+array.unshift(element);
+return array;
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  
 }
